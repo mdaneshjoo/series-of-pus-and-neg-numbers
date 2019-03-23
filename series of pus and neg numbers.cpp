f13312c6a7e16c70223fbb41f7cafcf1bf4c9eea
M.Daneshjoo <mdaneshjoo@M.Daneshjoo-UbuntuKDE>
2019-03-23 15:30:41 +0430

    wrong answer

---
+++ series of pus and neg numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+float anwr(int num)
+{    //s=1/2-2/3+3/4-4/5+...+(n-1)/n در اینجا مثبت و منفی را کی در نظر میگیریم(k)
+    float s;
+    int k;
+    for(int i = 1; i <= num; i++)
+    {
+        s=s+(k*(i/(i+1)));
+        k=-k;
+    }
+    return s;
+}
+int main()
+{
+    int num;
+    float b1;
+   
+    do
+    {
+        cin >> num;
+        b1 = num / 2;
+       
+    } while (b1 != floor(b1)); //اگر عدد وارد شده یه عدد زوج باشد اعشار نخواهد داشت و اگر اعشار داشته باشد یعنی عدد وارد شده فرد است
+    float a=anwr(num);
+    cout<<a;
+   
+}
